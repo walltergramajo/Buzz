a51c37dd11dd19ef7cd30a194c0409a80e09282f
Walter <waltergramajo@yahoo.ca>
2015-11-12 18:54:46 -0500

    Facebook Login

---
+++ Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Buzz
+//
+//  Created by Walter Gramajo on 2015-11-12.
+//  Copyright © 2015 Buzz. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* Bridging_Header_h */
--- Buzz/ViewController.swift
+++ Buzz/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if (FBSDKAccessToken.currentAccessToken() == nil)
+        {
+            print("Not logged in..")
+        }
+        else
+        {
+            print("Logged in..")
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        self.view.addSubview(loginButton)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        if error == nil
+        {
+            print("Login complete.")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        print("User logged out...")
+    }
+    
+    
+    
 }
 
